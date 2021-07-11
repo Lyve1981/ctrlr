91d910b6943570667d5390de11452b68f21571b3
Lyve <lyve2909+github@gmail.com>
2021-07-11 15:49:26 +0200

    remove "stdafx.h" from luabind, I mean, WTF

--- Source/Misc/luabind/src/exception_handler.cpp
+++ Source/Misc/luabind/src/exception_handler.cpp
@@ -1,7 +1,6 @@
 // Copyright Daniel Wallin 2005. Use, modification and distribution is
 // subject to the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-#include "stdafx.h"
 
 #define LUABIND_BUILDING
 #include <luabind/config.hpp>           // for LUABIND_API
--- Source/Misc/luabind/src/link_compatibility.cpp
+++ Source/Misc/luabind/src/link_compatibility.cpp
@@ -19,7 +19,6 @@
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
-#include "stdafx.h"
 
 #define LUABIND_BUILDING
 #include <luabind/detail/link_compatibility.hpp>
